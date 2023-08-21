4958c6bf45a0c6c2497951881b03d33459e54dd3
Anuj Agrawal <58907150+DanteAnuj@users.noreply.github.com>
2023-08-21 16:35:33 +0530

    Update Constants.swift
    I removed my API key so, I can publicize this repo.

--- youtube-build/Helpers/Constants.swift
+++ youtube-build/Helpers/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Constants {
     
-    static var API_KEY = "AIzaSyAJFVLoXq8rsnL7_Xd-jUU54lLsgZ9NFmk"
+    static var API_KEY = ""
     static var PLAYLIST_ID = "UULPsDTy8jvHcwMvSZf_JGi-FA"
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     static var VIDEOCELL_ID = "VideoCell"
